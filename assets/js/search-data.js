7220fc821d7aca26a3d857e76fd5992aec02471f
DarthUtopian <97091311+DarthUtopian@users.noreply.github.com>
2025-10-18 05:23:36 +0000

    Deploying to gh-pages from @ DarthUtopian/DarthUtopian.github.io@a3fbbede6d641aea5dd89aca2112d26dae895d54 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%76%6C%69%6E%33@%61%6E%64%72%65%77.%63%6D%75.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/DarthUtopian", "_blank");
         },
       },{
         id: 'social-rss',
